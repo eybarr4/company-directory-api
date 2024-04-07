51a8adc1b6968cd6259488429bc5248e42fdd824
eybarr4 <147701147+eybarr4@users.noreply.github.com>
2024-04-07 17:31:04 -0500

    Initial commit

---
+++ src/composables/useFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZcAPFq0gy1oHmPVHphxCX2DT9lXq8OwM",
+  authDomain: "company-directory-c543b.firebaseapp.com",
+  projectId: "company-directory-c543b",
+  storageBucket: "company-directory-c543b.appspot.com",
+  messagingSenderId: "391699083071",
+  appId: "1:391699083071:web:81e7fd7c46f31adee2ed8a"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
